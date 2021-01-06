d2a95fc4cd43de2314e7dbd71f565e25185dba36
Lance Fogle <lancefogle@users.noreply.github.com>
2021-01-06 16:55:50 -0500

    Updated command to resolve error

--- xo-install.sh
+++ xo-install.sh
@@ -590,7 +590,7 @@ function InstallXO {
 	if [[ "$XOUSER" != "root" ]]; then
 		echo
 		printprog "Adding sudo to mount command to allow mounting partitions as non-root user"
-		cmdlog "/usr/bin/sed -i \"s%execa('mount', [%execa('sudo', [\`mount\`, %\"" $INSTALLDIR/xo-builds/xen-orchestra-$TIME/packages/xo-server/src/xo-mixins/file-restore-ng.js"
+		#cmdlog "/usr/bin/sed -i "s%execa('mount', \[%execa('sudo', \[\`mount\`, %" $INSTALLDIR/xo-builds/xen-orchestra-$TIME/packages/xo-server/src/xo-mixins/file-restore-ng.js"
 		/usr/bin/sed -i "s%execa('mount', \[%execa('sudo', \[\`mount\`, %" $INSTALLDIR/xo-builds/xen-orchestra-$TIME/packages/xo-server/src/xo-mixins/file-restore-ng.js
 		printok "Adding sudo to mount command to allow mounting partitions as non-root user"
 
